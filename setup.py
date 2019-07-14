b81d4514dc8cf4f922afedf9660e301f098801cf
diego <diego.aliaga@helsinki.fi>
2019-07-14 17:07:58 -0400

    from_mac

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pi-vertical-garden',
+    version='0.0.0',
+    packages=['pi_vertical_garden'],
+    url='',
+    license='',
+    author='diego aliaga',
+    author_email='',
+    description=''
+)
